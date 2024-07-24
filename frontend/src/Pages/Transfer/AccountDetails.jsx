1145afb18d798ee29cc1c2de88722d647696f451
Antonio Navarro <anavarro67@hotmail.com>
2024-07-24 10:13:34 +0200

    feat: Validar nombre de contacto
    Se valida que si el nombre está informado tenga una longitud entre 3 y 20 caracteres y que no tenga caracters especiales. En caso de error se muestra mensaje descriptivo debajo del campo.

--- frontend/src/Pages/Transfer/AccountDetails.jsx
+++ frontend/src/Pages/Transfer/AccountDetails.jsx
@@ -1,8 +1,69 @@
 
 import Header from '../../components/Header'
+import { useState } from 'react'
+
+const AccountDails = ({updateFormData, setIsValidForm}) => {
 
 
-const AccountDails = () => {
+  const [iban, setIban] = useState()
+  const [ibanerror, setIbanError] = useState('')
+  const [contactNameError, setContactNameError] = useState('')
+  const [bankNameError, setBankError] = useState('')
+  
+  const [validForm, setValidForm] = useState(false)
+
+
+  const checkIsValidForm = () => {
+    
+    if (!ibanerror && !contactNameError && !bankNameError) {      
+      setIsValidForm(true)      
+    } else {
+      setIsValidForm(false)      
+    }
+  }
+
+  // Validaciones IBAN
+  const handleIBAN = (event) => {
+    if (!event.target.value) {
+        setIbanError('Debe indicar un numero de cuenta valido')
+    } else {
+      setIbanError('')
+      checkIsValidForm()
+    }
+  }
+
+  // Validaciones nombre de contacto
+  const handleContactName = (event) => {
+
+    
+    
+    let nombreContact = event.target.value
+    
+    const regex = /^[A-Z][a-zA-Z-' ]*$/;
+
+    if (nombreContact) {
+      
+      if (nombreContact.length < 3 || nombreContact.length > 20) {
+        
+        setContactNameError('El nombre del contacto debe de tener entre 3 y 20 caracteres')
+      } else if (!regex.test(nombreContact)){
+        setContactNameError('El nombre del contacto solo puede contener letras')
+      } else {
+        setContactNameError('')
+      }
+
+
+    } else {
+      setContactNameError('')
+    }
+
+
+  }
+
+
+  
+  
+  
   return (
     <>
 
@@ -14,36 +75,49 @@ const AccountDails = () => {
 
         <h2 className='text-3xl'> Agrega un numero de cuenta </h2>
 
-        <div className='mx-auto w-11/12'>
-          <label className='block mb-2 text-sm font-bold text-gray-700'> Numero de IBAN </label>
-          <div className='flex items-center mb-6 border rounded shadow  bg-greyDesign'>
+        <div className='mx-auto w-11/12 flex flex-col'>
+            
+                <label className='block mb-2 text-sm font-bold text-gray-700'> Numero de IBAN </label>
+            
+                <div className='flexf items-center mb-1 border rounded shadow  bg-greyDesign'>
+{/*IBAN */}
+                <input type='text' 
+                  name="IBAN"
+                  className='w-full p-4 leading-tight bg-lightGrey text-gray-700 border-none focus:outline-none focus:shadow-outline'
+                  placeholder='Ingresa numero de IBAN' 
+                  onBlur={handleIBAN}/>
+                
+                </div>
+                {ibanerror && <div> <p className='text-sm text-red-600'>  {ibanerror} </p> </div>}
+              
 
-            <input type='text' className='w-full p-4 leading-tight bg-lightGrey text-gray-700 border-none focus:outline-none focus:shadow-outline'
-              placeholder='Ingresa numero de IBAN' />
-          </div>
+
+          
+
         </div>
-        {/* <div> 
-      <label htmlFor=""></label>
-      <input type="text" 
-      placeholder='Ingresa numero de IBAN'/>
-    </div> */}
 
 
         <div className='mx-auto w-11/12'>
           <p>  ¿Deseas guardar este contacto como transferencia frecuente? </p>
+{/* Nombre de contacto */}          
           <label htmlFor="" className='text-primary'> Nombre de contacto </label>
           <input
+            name='contactName'
             type="text"
             placeholder='Ingresa el nombre del contacto'
             className='w-full p-4 leading-tight bg-lightGrey text-gray-700 border-none focus:outline-none focus:shadow-outline mb-[16px]'
+            onBlur={handleContactName}
           />
+          {contactNameError && <div> <p className='text-sm text-red-600'>  {contactNameError} </p> </div>}
 
           <label htmlFor="" className='text-primary'> Nombre de banco destino </label>
 
           <input
+            name='destinationBank'
             type="text"
             placeholder='Ingresa el nombre del banco destino'
             className='w-full p-4 leading-tight bg-lightGrey text-gray-700 border-none focus:outline-none focus:shadow-outline'
+            onChange={updateFormData}
           />
 
         </div>
@@ -55,12 +129,6 @@ const AccountDails = () => {
 
 
 
-        {/* <div className='w-full px-6 pb-4'>
-        <div className='flex justify-end gap-3 items-center'>
-          <p> Atras </p>
-          <button className='bg-primary text-white  py-2 px-8 rounded-lg shadow' > Siguiente </button>
-          </div>
-      </div> */}
 
       </div>
 
